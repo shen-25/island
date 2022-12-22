7961b82ee6792dc423730333afdacf97a6b72b7c
zengshen <3553642457@qq.com>
2022-12-22 11:43:21 +0800

    like组件的基本完成，学到了自定义组件

---
+++ components/like/index.js
@@ -0,0 +1,37 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like:{
+      type: Boolean
+    },
+    count:{
+      type: Number
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   * 组件内部使用在这里定义
+   */
+  data: {
+    //数据绑定
+    yesSrc: './images/like.png',
+    disSrc: './images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike: function(event){
+      let like = this.properties.like
+      console.log(like)
+      like = !like
+      console.log(like)
+    }
+
+  }
+})
